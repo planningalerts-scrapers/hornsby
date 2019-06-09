f90ee260c8988aafd27447ff8c06fcbd6ead2a16
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 01:39:58 +1000

    Update to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,3 @@
 require "icon_scraper"
 
-IconScraper.rest_xml(
-  "http://hscenquiry.hornsby.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx",
-  "d=last14days&k=LodgementDate&o=xml",
-  false
-)
+IconScraper.scrape_and_save(:hornsby)
